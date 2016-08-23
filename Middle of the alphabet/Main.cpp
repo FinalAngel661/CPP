e18db932cd3566fc223280887de28843fe3d4e21
kelsey.davis <kelsey.davis@students.aie.edu.au>
2016-08-23 10:32:21 -0700

    Added a new script called Middle Letter.

--- Middle of the alphabet/Main.cpp
+++ Middle of the alphabet/Main.cpp
@@ -1,3 +1,54 @@
 
 #include <iostream>
 
+int main()
+{
+	
+	printf("Enter 2 letters in the alphabet to find the middle letter between them.\n");
+	
+	if
+	{
+
+		char a, b;
+		scanf_s("%c %c", &a, 1, &b, 1);
+		getchar();
+
+		int middle = (a + b) / 2;
+
+		printf("The middle letter is: %c", middle);
+		getchar();
+
+		else
+		{
+			char A, B;
+			scanf_s("%c %c", &A, 1, &B, 1);
+			getchar();
+
+			int middle = (A + B) / 2;
+
+			printf("The middle letter is: %c", middle);
+			getchar();
+
+		};;
+	
+	/*
+	char a, b;
+	scanf_s("%c %c", &a, 1, &b, 1, &A, 1, &B, 1);
+	getchar();
+
+	int middle = (a + b) / 2;
+
+	printf("The middle letter is: %c", middle);
+	getchar();
+	*/
+	
+
+	/*
+	char ch = 0;
+	scanf_s("%c", &ch,1);
+	int ival = ch;
+	printf("%d\n", ival);
+	getchar();
+	system("pause");
+	*/
+}
